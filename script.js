5e2bbfd0d38255b57f4d681cc94ef5ccbfb58f00
Thapelo A Motsumi <Thapelo.umuziphotoclub@gmail.com>
2023-04-18 15:28:04 +0200

    add changes into my javascript

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
 const subtractHandler =() =>{
+    number.value = number.value - 2;
 
 }
 
 
-const addtractHandler =() =>{
-
+const addHandler =() =>{
+    number.value = number.value + 40;
 }
 
 subtract.addEventListener('click', subtractHandler)
